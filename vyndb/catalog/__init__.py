eb5c46c80b32929053d4df5633704153c01be76b
IshaanShivalli <smilewithishaan25@gmail.com>
2026-07-08 17:58:11 +0530

    Removed VYNDB

--- vyndb/catalog/__init__.py
+++
@@ -1,8 +0,0 @@
-from catalog.schema import ColumnDef, TableDef, IndexDef, SUPPORTED_TYPES
-from catalog.catalog import Catalog, CatalogError
-
-__all__ = [
-    'Catalog', 'CatalogError',
-    'ColumnDef', 'TableDef', 'IndexDef',
-    'SUPPORTED_TYPES',
-]
